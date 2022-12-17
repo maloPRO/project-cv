57d8c38c176e8101784181f86d12aa807cc8dd89
Gilbert Malova <malovagilbert@gmail.com>
2022-12-17 12:41:09 +0300

    styled personal and contact section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import PersonalInfo from './components/personal';
+import './styles/index.css';
+import Contacts from './components/contacts';
 
+const user = {
+  name: 'Gilbert Malova',
+  title: 'Software Developer'
+}
+
+const contacts = {
+  tel: '+254 792 184 923',
+  email: 'malovagilbert@gmail.com',
+  address: 'Eldoret, Kenya',
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <div className='left'>
+      <PersonalInfo user = {user}/>
+      <Contacts contacts = {contacts} />
+    </div>
+    <div className='right'>
+    </div>
   </React.StrictMode>
 );
 
