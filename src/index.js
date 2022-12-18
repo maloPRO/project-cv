2ece96078cfb3512c8df94386f2dea0db042c966
Gilbert Malova <malovagilbert@gmail.com>
2022-12-18 17:00:50 +0300

    rendered referees to page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import PersonalInfo from './components/personal';
 import './styles/index.css';
 import Contacts from './components/contacts';
 import Interests from './components/interests';
+import References from './components/references';
 
 const user = {
   name: 'Gilbert Malova',
@@ -18,7 +19,29 @@ const contacts = {
 
 const interests = ['Coding', 'Reading', 'Playing Chess', 'Hiking'];
 
-
+const referees = [
+  {
+    title: 'Mr.',
+    names: 'Mate Boniface',
+    org: 'Childrens Transformation Project',
+    phone: '+254 719 875 689',
+    mail: 'mateboniface@gmail.com'
+  },
+  {
+    title: 'Mrs.',
+    names: 'Lynett Odida',
+    org: 'Ministry of Labor',
+    phone: '+254 727 948 443',
+    mail: 'lodida78@yahoo.com'
+  },
+  {
+    title: 'Mr.',
+    names: 'Byrum Charles',
+    org: 'Cubahiro International School',
+    phone: '+257 435 553 432',
+    mail: 'byrumcharles@gmail.com'
+  }
+]
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -26,6 +49,7 @@ root.render(
       <PersonalInfo user = {user}/>
       <Contacts contacts = {contacts} />
       <Interests interests = {interests} />
+      <References referees = {referees} />
     </div>
     <div className='right'>
     </div>
