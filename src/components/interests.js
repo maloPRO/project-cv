5bf49bb9da17ce16a8a7169c1f51e2370f24d31a
Gilbert Malova <malovagilbert@gmail.com>
2022-12-18 14:18:17 +0300

    Added interests section

---
+++ src/components/interests.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import '../styles/interests.css';
+
+class Interests extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      interests: this.props.interests
+    }
+     }
+
+     render() {
+      const interests = this.props.interests;
+      const interestList = interests.map((interest) => 
+        <li>{interest}</li>
+      )
+      return (
+        <div className="interests">
+          <h3>Interests</h3>
+          <ul>{interestList}</ul>
+        </div>
+      )
+     }
+
+
+}
+export default Interests;
