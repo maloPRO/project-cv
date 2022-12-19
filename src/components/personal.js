274cfe616ff310b43b59f241d2b96f7e82864dfe
Gilbert Malova <malovagilbert@gmail.com>
2022-12-19 11:44:53 +0300

    Added edit personal information  values functionality and styling

--- src/components/personal.js
+++ src/components/personal.js
@@ -1,23 +1,37 @@
 import React, { Component } from "react";
+import { EditText } from 'react-edit-text';
 import '../styles/personalInfo.css';
 import sample from '../images/sample.jpg';
+
 
 class PersonalInfo extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      name: this.props.name,
-      title: this.props.title,
-      profile: this.props.profile
+      user: {
+        name: this.props.name,
+        title: this.props.title,
+        profile: this.props.profile
+      }
     }
   }
   render () {
     return (
       <div className="personalInfo">
         <img src={sample} className='dp' alt=""></img>
-        <p className="name">{this.props.user.name} </p>
-        <p className="title">{this.props.user.title}</p>
+        <EditText
+         name="name"
+         defaultValue={this.props.user.name}
+         inputClassName = 'nameInput'
+         className="name"
+         />
+        <EditText
+         name="name"
+         defaultValue={this.props.user.title}
+         inputClassName = 'titleInput'
+         className="title"
+         />
       </div>
     )
   }
