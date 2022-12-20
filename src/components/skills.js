9a6bbd0fa31c37776f5b274d7f4bf749ab0f7022
Gilbert Malova <malovagilbert@gmail.com>
2022-12-20 22:00:01 +0300

    Added skills section

---
+++ src/components/skills.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { EditText } from 'react-edit-text';
+import '../styles/skills.css';
+import skillICon from '../images/skill.png';
+
+class Skills extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      skills: this.props.skills
+    }
+     }
+
+     render() {
+      const skills = this.props.skills;
+      const skillsList = skills.map((skill) => 
+        <li key={skill.id}>
+          <EditText
+          name="skills"
+          defaultValue= {skill.name}
+          inputClassName = 'interestInput'
+        />
+        </li>
+      )
+      return (
+        <div className="skills">
+          <div className="skill"><img alt="" className="icon" src={skillICon}></img> Skills</div>
+          <ul>{skillsList}</ul>
+        </div>
+      )
+     }
+}
+export default Skills;
