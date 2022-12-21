3721c9ac8e2b7da45d2fb5ff28c9ba02d9b2dd9a
Gilbert Malova <malovagilbert@gmail.com>
2022-12-21 13:49:38 +0300

    Added delete and add new interests features

--- src/components/interests.js
+++ src/components/interests.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { EditText } from 'react-edit-text';
 import '../styles/interests.css';
+import uniqid from 'uniqid'
 
 class Interests extends Component {
   constructor(props) {
@@ -9,25 +10,55 @@ class Interests extends Component {
     this.state = {
       interests: this.props.interests
     }
+    
      }
+
+     delete = (e) => {
+      e.target.parentElement.remove()
+    }
+    show = (e) => {
+      e.target.querySelector('button').className = 'show';
+      e.target.querySelector('#addInterest').className =  'show';
+    }
+    hide = (e) => {
+      e.target.querySelector('button').className = 'hide';
+      e.target.querySelector('#addInterest').className =  'hide';
+    }
+    add = (e) => {
+      this.setState({
+        interests: this.props.interests.push({name: 'New Interest', id: uniqid()})
+      })
+    }
 
      render() {
       const interests = this.props.interests;
       const interestList = interests.map((interest) => 
-        <li key={interest.id}>
-          <EditText
-          name="interests"
-          defaultValue= {interest.name}
-          inputClassName = 'interestInput'
-        />
+        <li
+          onMouseEnter={this.show}
+          onMouseLeave = {this.hide} 
+          key={interest.id}>
+            <EditText
+            name="interests"
+            defaultValue= {interest.name}
+            inputClassName = 'interestInput'
+         />
+         <button 
+          className="hide"
+          onClick={this.delete}
+          ></button>
         </li>
       )
       return (
-        <div className="interests">
+        <div 
+          onMouseEnter={this.show}
+          onMouseLeave = {this.hide}
+          className="interests">
           <h3>Interests</h3>
           <ul>{interestList}</ul>
+          <button id="addInterest" className="hide" onClick={this.add}>Add interest</button>
         </div>
       )
      }
 }
+
 export default Interests;
