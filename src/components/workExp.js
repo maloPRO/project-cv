1eaabdabcfc6f56c2c271ca0095a8481db0ada55
Gilbert Malova <malovagilbert@gmail.com>
2022-12-21 14:44:14 +0300

    Added delete work experience functionality

--- src/components/workExp.js
+++ src/components/workExp.js
@@ -10,18 +10,35 @@ class Work extends Component {
     this.state = {
       jobs: this.props.jobs,
     }
-  }
 
+  }
+  deletejob = (e) => {
+    e.target.parentElement.parentElement.parentElement.remove();  
+  }
+  hideDeleteBtn = (e) => {
+    e.target.querySelector('#deleteJob').className = 'hide'
+  }
+  showDeleteBtn = (e) => {
+    e.target.querySelector('#deleteJob').className = 'show'
+  }
   render() {
     const jobs = this.props.jobs;
     const jobList = jobs.map((job) => 
-      <div key={job.id} className='job'>
+      <div 
+        key={job.id} 
+        className='job'
+        onMouseEnter={this.showDeleteBtn}
+        onMouseLeave={this.hideDeleteBtn}
+        >
         <div>
           <div className="title"><EditText inputClassName="workInput" defaultValue={job.title}/></div>
           <div className="date"><EditText inputClassName="workInput" defaultValue={job.date} /></div>
         </div>
         <div>
-          <div className="org"><EditText defaultValue={job.org} /></div>
+          <div className="org">
+            <EditText defaultValue={job.org} />
+            <button id="deleteJob" onClick={this.deletejob} className="hide"></button>
+          </div>
           <div className="roles"><span>Duties & Responsibilities</span><ul>{job.roles.map((role) => <li key={role.id}><EditText inputClassName="roleInput" defaultValue={role.name} /></li>)}</ul></div>
         </div>
       </div>
